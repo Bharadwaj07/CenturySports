d3023caefdfa2147f68ca79528c7b66aad5d12ca
unknown <aishwaryapauskar6@gmail.com>
2020-04-11 03:07:44 +0530

    feature-landing added Trending and Headline services

---
+++ src/app/service/sports/sportsTopHeadLinesData.ts
@@ -0,0 +1,18 @@
+export const sportsTopHeadLinesData=[
+    {
+        headLineId:"spHeadLine1",
+        headLineData:"England plan to play different formats on consecutive days more likely than on same day, says Ashley Giles"
+    },
+    {
+        headLineId:"spHeadLine2",
+        headLineData:"Jadon Sancho’s stance on Manchester United move as Dortmund fire transfer warning"
+    },
+    {
+        headLineId:"spHeadLine3",
+        headLineData:"Not sure who went 'easy' on Virat Kohli for IPL contract - Tim Paine"
+    },
+    {
+        headLineId:"spHeadLine4",
+        headLineData:"Andy Murray signs up for virtual Madrid Open"
+    },
+]
