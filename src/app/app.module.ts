e5c95f9af0c4f53739cd1facfd3750ac974865f5
unknown <aishwaryapauskar6@gmail.com>
2020-04-11 01:11:39 +0530

    feature-landing changed agmcoremodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HighlightsComponent } from './highlights/highlights.component';
 import { SafePipe } from './service/pipe/safe.pipe';
 import { LogoutComponent } from './logout/logout.component';
 import { LiveComponent } from './live/live.component';
-import { AgmCoreModule } from '@agm/core';
+
 import { HeaderComponent } from './shell/header/header.component';
 import { FooterComponent } from './shell/footer/footer.component';
 import { HomeCarouselComponent } from './home/home-carousel/home-carousel.component';
@@ -43,9 +43,7 @@ import { DashboardJumbotronComponent } from './home/dashboard-jumbotron/dashboar
     HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyD-dIA5uX4cj12aUCEb8GlToD-jYvil42s'
-    }),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
