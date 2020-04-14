02718b333609962c08fe3b4c8546c96fc06d3f23
Bharadwaj K R <yajamankr@gmail.com>
2020-04-14 15:28:11 +0530

    small changes

--- src/app/service/sports/sportsTrendingData.ts
+++ src/app/service/sports/sportsTrendingData.ts
@@ -1,22 +1,68 @@
-export const sportsTrendingData=[
-    {
-        caption:"Germany Prepares for Soccer’s Return",
-        imgUrl:"spSocker.jpeg",
-        displayText:"Germany’s return to the field will be watched closely. All European soccer has been halted, with the strange exception of Belarus. FIFA is allowing an indefinite extension to let leagues around the world complete their stopped-in-time seasons."
-    },
-    {
-        caption:"T20I in Hamilton",
-        imgUrl:"spCricket.jpeg",
-        displayText:"India's run-machine opener Rohit Sharma stole the headlines as the visitors prevailed over New Zealand in a thrilling Super Over in the third T20I in Hamilton"
-    },
-    {
-        caption:"Rafael Nadal",
-        imgUrl:"spTennis.jpeg",
-        displayText:"Rafael Nadal wins 19th Grand Slam, is now one behind Roger Federer."
-    },
-    {
-        caption:"ISL 2019-20",
-        imgUrl:"spFootBall.jpeg",
-        displayText:"ISL 2019-20: Dedication, ambition and Antonio Habas' tactical acumen lace ATK's glory of winning third title"
-    }
-]
+export const sportsTrendingData={
+    cricket:[
+        {
+            caption:"West Indies v England Test Series ",
+            imgUrl:"spSocker.jpeg",
+            displayText:"West Indies Test captain Jason Holder wants his team to make amends for their 2017 defeat in England, should their planned Test tour for this summer go ahead. Holder is confident that he and his men now have the experience as well as confidence to win a Test series in England, a feat last achieved in 1988 when Viv Richards' team blanked the hosts 4-0 in the five-Test series."
+        },
+        {
+            caption:"T20I in Hamilton",
+            imgUrl:"spCricket.jpeg",
+            displayText:"India's run-machine opener Rohit Sharma stole the headlines as the visitors prevailed over New Zealand in a thrilling Super Over in the third T20I in Hamilton"
+        },
+        {
+            caption:"Ind v NZ 2nd ODI",
+            imgUrl:"spTennis.jpeg",
+            displayText:"Rafael Nadal wins 19th Grand Slam, is now one behind Roger Federer."
+        },
+        {
+            caption:"ISL 2019-20",
+            imgUrl:"spFootBall.jpeg",
+            displayText:"ISL 2019-20: Dedication, ambition and Antonio Habas' tactical acumen lace ATK's glory of winning third title"
+        }
+    ],
+    football:[
+        {
+            caption:"Germany Prepares for Soccer’s Return",
+            imgUrl:"spSocker.jpeg",
+            displayText:"Germany’s return to the field will be watched closely. All European soccer has been halted, with the strange exception of Belarus. FIFA is allowing an indefinite extension to let leagues around the world complete their stopped-in-time seasons."
+        },
+        {
+            caption:"T20I in Hamilton",
+            imgUrl:"spCricket.jpeg",
+            displayText:"India's run-machine opener Rohit Sharma stole the headlines as the visitors prevailed over New Zealand in a thrilling Super Over in the third T20I in Hamilton"
+        },
+        {
+            caption:"Rafael Nadal",
+            imgUrl:"spTennis.jpeg",
+            displayText:"Rafael Nadal wins 19th Grand Slam, is now one behind Roger Federer."
+        },
+        {
+            caption:"ISL 2019-20",
+            imgUrl:"spFootBall.jpeg",
+            displayText:"ISL 2019-20: Dedication, ambition and Antonio Habas' tactical acumen lace ATK's glory of winning third title"
+        }
+    ],
+    hockey:[
+        {
+            caption:"Germany Prepares for Soccer’s Return",
+            imgUrl:"spSocker.jpeg",
+            displayText:"Germany’s return to the field will be watched closely. All European soccer has been halted, with the strange exception of Belarus. FIFA is allowing an indefinite extension to let leagues around the world complete their stopped-in-time seasons."
+        },
+        {
+            caption:"T20I in Hamilton",
+            imgUrl:"spCricket.jpeg",
+            displayText:"India's run-machine opener Rohit Sharma stole the headlines as the visitors prevailed over New Zealand in a thrilling Super Over in the third T20I in Hamilton"
+        },
+        {
+            caption:"Rafael Nadal",
+            imgUrl:"spTennis.jpeg",
+            displayText:"Rafael Nadal wins 19th Grand Slam, is now one behind Roger Federer."
+        },
+        {
+            caption:"ISL 2019-20",
+            imgUrl:"spFootBall.jpeg",
+            displayText:"ISL 2019-20: Dedication, ambition and Antonio Habas' tactical acumen lace ATK's glory of winning third title"
+        }
+    ],
+}
